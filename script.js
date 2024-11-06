277492ff0bfedd804609cf9acc20e15c8194502b
Gabriela Ramires <engenheira.ramires@gmail.com>
2024-11-06 00:47:33 -0300

    todas cartas incluidas

--- script.js
+++ script.js
@@ -13,12 +13,11 @@ new Vue({
     iniciar() {
       console.log(this.categoria)
       if(this.isMobile) {
-        this.criarCartasMobile(78);
+        this.criarCartasMobile(22);
       } else {
-        this.criarCartas(3);
-        this.buscarCartasServidor();
-        console.log(this.cartas)
+        this.criarCartas(22);
       }
+      this.buscarCartasServidor();
       
     },
     criarCartas(numCartas) {
@@ -27,7 +26,7 @@ new Vue({
       const cartasPorBaralho = Math.ceil(numCartas / numBaralhos); // Quantidade de cartas por baralho
       const cartas = [];
       const offsetX = -20; // Distância entre colunas
-      const offsetY = 20; // Distância entre linhas
+      const offsetY = 45; // Distância entre linhas
 
       for (let i = 1; i <= numCartas; i++) {
           const baralhoIndex = Math.floor((i - 1) / cartasPorBaralho); // Índice do baralho
@@ -35,7 +34,7 @@ new Vue({
 
           // Calcula as posições
           const bottom = baralhoIndex * offsetX; // Posição horizontal
-          const left = 200 + (posInBaralho * offsetY); // Posição vertical
+          const left = 0 + (posInBaralho * offsetY); // Posição vertical
 
           cartas.push({ id: i, left: left, bottom: bottom, msg: '', img: '', idReal: '', titulo: ''});
       }
@@ -45,21 +44,21 @@ new Vue({
     },
     criarCartasMobile(numCartas) {
       // Cria as cartas dinamicamente
-      const numBaralhos = 3; // Número de baralhos
+      const numBaralhos = 2; // Número de baralhos
       const cartasPorBaralho = Math.ceil(numCartas / numBaralhos); // Quantidade de cartas por baralho
       const cartas = [];
-      const offsetX = 120; // Distância entre colunas
+      const offsetX = -150; // Distância entre colunas
       const offsetY = 20; // Distância entre linhas
 
       for (let i = 1; i <= numCartas; i++) {
-        const baralhoIndex = Math.floor((i - 1) / cartasPorBaralho); // Índice do baralho
-        const posInBaralho = (i - 1) % cartasPorBaralho; // Posição dentro do baralho
+          const baralhoIndex = Math.floor((i - 1) / cartasPorBaralho); // Índice do baralho
+          const posInBaralho = (i - 1) % cartasPorBaralho; // Posição dentro do baralho
 
-        // Calcula as posições
-        const left = baralhoIndex * offsetX +90 ; // Posição horizontal
-        const bottom = 200 - (posInBaralho * offsetY); // Posição vertical
+          // Calcula as posições
+          const bottom = 260 + (baralhoIndex * offsetX); // Posição horizontal
+          const left = 80 + (posInBaralho * offsetY); // Posição vertical
 
-        cartas.push({ id: i, left: left, bottom: bottom });
+          cartas.push({ id: i, left: left, bottom: bottom, msg: '', img: '', idReal: '', titulo: ''});
       }
 
       this.cartas = cartas;
--- tarot.php
+++ tarot.php
@@ -42,9 +42,29 @@ class Tarot{
         $response = [
             'status' => true,
             'data' => [
-                ['idReal' => 1, 'titulo' => 'O louco', 'img' => 'o-louco.jpg', 'msg' => 'mensagem do louco'],
-                ['idReal' => 2, 'titulo' => 'O mago', 'img' => 'o-mago.jpg', 'msg' => 'mensagem do mago'],
+                ['idReal' => 1, 'titulo' => 'O Louco', 'img' => 'o-louco.jpg', 'msg' => 'mensagem do louco'],
+                ['idReal' => 2, 'titulo' => 'O mMgo', 'img' => 'o-mago.jpg', 'msg' => 'mensagem do mago'],
                 ['idReal' => 3, 'titulo' => 'A Sacerdotisa', 'img' => 'a-sacerdotisa.jpg', 'msg' => 'mensagem da sacedotisa'],
+                ['idReal' => 4, 'titulo' => 'A Imperatriz', 'img' => 'a-imperatriz.jpg', 'msg' => 'mensagem da imperatriz'],
+                ['idReal' => 5, 'titulo' => 'O Imperador', 'img' => 'o-imperador.jpg', 'msg' => 'mensagem do imperador'],
+                ['idReal' => 6, 'titulo' => 'O Papa', 'img' => 'o-papa.jpg', 'msg' => 'mensagem do papa'],
+                ['idReal' => 7, 'titulo' => 'Os Enamorados', 'img' => 'os-enamorados.jpg', 'msg' => 'mensagem dos enamorados'],
+                ['idReal' => 8, 'titulo' => 'O Carro', 'img' => 'o-carro.jpg', 'msg' => 'mensagem do carro'],
+                ['idReal' => 9, 'titulo' => 'A força', 'img' => 'a-forca.jpg', 'msg' => 'mensagem da força'],
+                ['idReal' => 10, 'titulo' => 'O Eremita', 'img' => 'o-eremita.jpg', 'msg' => 'mensagem do erimita'],
+                ['idReal' => 11, 'titulo' => 'A Roda da Fortuna', 'img' => 'a-roda-da-fortuna.jpg', 'msg' => 'mensagem da roda da fortuna'],
+                ['idReal' => 12, 'titulo' => 'A Justiça', 'img' => 'a-justica.jpg', 'msg' => 'mensagem da justiça'],
+                ['idReal' => 12, 'titulo' => 'A Justiça', 'img' => 'a-justica.jpg', 'msg' => 'mensagem da justiça'],
+                ['idReal' => 13, 'titulo' => 'O Enforcado', 'img' => 'o-enforcado.jpg', 'msg' => 'mensagem do enforcado'],
+                ['idReal' => 14, 'titulo' => 'A Morte', 'img' => 'a-morte.jpg', 'msg' => 'mensagem da morte'],
+                ['idReal' => 15, 'titulo' => 'A Temperança', 'img' => 'a-temperanca.jpg', 'msg' => 'mensagem da temperança'],
+                ['idReal' => 16, 'titulo' => 'O Diabo', 'img' => 'o-diabo.jpg', 'msg' => 'mensagem do diabo'],
+                ['idReal' => 17, 'titulo' => 'A Torre', 'img' => 'a-torre.jpg', 'msg' => 'mensagem da torre'],
+                ['idReal' => 18, 'titulo' => 'A Estrela', 'img' => 'a-estrela.jpg', 'msg' => 'mensagem da estrela'],
+                ['idReal' => 19, 'titulo' => 'A Lua', 'img' => 'a-lua.jpg', 'msg' => 'mensagem da lua'],
+                ['idReal' => 20, 'titulo' => 'O Sol', 'img' => 'o-sol.jpg', 'msg' => 'mensagem do sol'],
+                ['idReal' => 21, 'titulo' => 'O Julgamento', 'img' => 'o-julgamento.jpg', 'msg' => 'mensagem do julgamento'],
+                ['idReal' => 22, 'titulo' => 'O Mundo', 'img' => 'o-mundo.jpg', 'msg' => 'mensagem do mundo']
             ]
         ];   
 
